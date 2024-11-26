976503f3a59620d965ab95e692442c1bc1c028db
munni234 <161602125+munni234@users.noreply.github.com>
2024-11-26 22:36:07 +0500

    a static resume builder

---
+++ main.js
@@ -0,0 +1,5 @@
+var skillsec = document.getElementById("skills");
+var toggleBtn = document.getElementById("hide-btn");
+toggleBtn.addEventListener("click", function () {
+    skillsec.classList.toggle("hidden");
+});
